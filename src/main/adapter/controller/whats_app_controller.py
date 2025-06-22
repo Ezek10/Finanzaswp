2ea94118a177918facf1de332aac7e8579b58995
Ezek10 <ezequielmarcel2@gmail.com>
2025-06-22 14:37:58 -0300

    remove bad response

--- src/main/adapter/controller/whats_app_controller.py
+++ src/main/adapter/controller/whats_app_controller.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from fastapi import Depends, Request, Response
+from fastapi import Depends, Request, Response, BackgroundTasks
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
 
@@ -19,17 +19,16 @@ async def verification(request: Request):
 
 
 @router.post("")
-async def read_message(request: dict, session = Depends(get_db)):
-    print(f"Request: {request}")
-    try:
+async def read_message(request: dict, background_tasks: BackgroundTasks, session = Depends(get_db)):
+    def task(request: dict, session):
+        print(f"Request: {request}")
         request_parsed = WPRequest(**request)
-    except Exception as e:
-        return JSONResponse(status_code=422, content={"error": str(e)})
-    message = request_parsed.entry[0].changes[0].value.messages[0]
-    message_id = message.id
-    phone = message.from_
-    timestamp = int(message.timestamp)
-    body = message.text.body
-    date = datetime.fromtimestamp(timestamp)
-    ProcessMessageUseCase(session).execute(phone=phone, message=body, date=date, message_id=message_id)
+        message = request_parsed.entry[0].changes[0].value.messages[0]
+        message_id = message.id
+        phone = message.from_
+        timestamp = int(message.timestamp)
+        body = message.text.body
+        date = datetime.fromtimestamp(timestamp)
+        ProcessMessageUseCase(session).execute(phone=phone, message=body, date=date, message_id=message_id)
+    background_tasks.add_task(task, request, session)
     return JSONResponse(status_code=200, content="")
--- src/main/domain/exceptions/handler.py
+++ src/main/domain/exceptions/handler.py
@@ -12,8 +12,6 @@ def ProcessException(request: Request, exception: Exception) -> JSONResponse:
     except ApplicationException as ex:
         print(f"ApplicationException: {ex.phone} - {ex.args[0]}")
         send_message(ex.phone, ex.args[0])
-        return JSONResponse(status_code=200, content="")
 
     except Exception as ex:
-        print(f"ERROR {ex}")
-        return JSONResponse(status_code=500, content="Internal Server Error")
+        print(f"ERROR: {ex}")
